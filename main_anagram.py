18991b14c0c9f2257f64c186a8681b25300b31f2
DoeIsaac <idoe.martu@gmail.com>
2022-05-28 10:31:54 +0100

    first file for my Zuri Program

---
+++ main_anagram.py
@@ -0,0 +1,22 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    
+    if (len(word)==len(anagram)):
+        w = word.lower()
+        a = anagram.lower()
+        if sorted(w)== sorted(a):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+word = input("Enter word: ")
+anagram = input("Enter next word: ")
+print(find_anagram(word, anagram))
